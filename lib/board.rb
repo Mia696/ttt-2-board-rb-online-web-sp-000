2c21c7213f1c00919342edefbf0308ff5b739b49
Mia <tamiapaschall@gmail.com>
2019-10-12 21:44:29 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,9 +1,9 @@
 # Define the variable board below.
 def display_board
-  puts "  |     |  "
-  puts "___________"
-  puts " |     |   " 
-  puts "___________"
-  puts " |     |   " 
+  puts "  |     |   "
+  puts "___________ "
+  puts "  |     |   " 
+  puts "___________ "
+  puts "  |     |   " 
 end  
   
